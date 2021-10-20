319cfb1b10066e2918b0f628bb1d14c8faabe56c
zjx1319 <897223895@qq.com>
2021-10-20 18:57:36 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module Project1
+
+go 1.17
+
+require github.com/labstack/echo v3.3.10+incompatible
+
+require (
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+var data = make(map[string]string)
+
+type DataJson struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/api/value/:key", getValue)
+	e.POST("/api/value/:key",setValue)
+	e.DELETE("/api/value/:key",deleteValue)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getValue(c echo.Context) (err error) {
+	var dataJson DataJson
+	var is bool
+	dataJson.Key = c.Param("key")
+	dataJson.Value, is = data[dataJson.Key]
+	if is {
+		return c.JSON(http.StatusOK,dataJson)
+	} else {
+		return c.String(http.StatusOK, "not found")
+	}
+}
+
+func setValue(c echo.Context) (err error) {
+	dataJson:=new(DataJson)
+	dataJson.Key = c.Param("key")
+	if err = c.Bind(dataJson); err != nil {
+		return
+	}
+	data[dataJson.Key] = dataJson.Value
+	return c.String(http.StatusOK, "success")
+}
+
+func deleteValue(c echo.Context) (err error) {
+	key := c.Param("key")
+	delete(data,key)
+	return c.String(http.StatusOK, "success")
+}
