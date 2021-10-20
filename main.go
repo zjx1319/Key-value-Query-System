185ea1a4fa941f9e9b660a2e68d1bd8df52fb814
zjx1319 <897223895@qq.com>
2021-10-20 20:59:31 +0800

    Changes

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ type DataJson struct {
 func main() {
 	e := echo.New()
 	e.GET("/api/value/:key", getValue)
-	e.POST("/api/value/:key",setValue)
-	e.DELETE("/api/value/:key",deleteValue)
+	e.POST("/api/value/:key", setValue)
+	e.DELETE("/api/value/:key", deleteValue)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -27,17 +27,21 @@ func getValue(c echo.Context) (err error) {
 	dataJson.Key = c.Param("key")
 	dataJson.Value, is = data[dataJson.Key]
 	if is {
-		return c.JSON(http.StatusOK,dataJson)
+		return c.JSON(http.StatusOK, dataJson)
 	} else {
 		return c.String(http.StatusOK, "not found")
 	}
 }
 
 func setValue(c echo.Context) (err error) {
-	dataJson:=new(DataJson)
+	dataJson := new(DataJson)
 	dataJson.Key = c.Param("key")
 	if err = c.Bind(dataJson); err != nil {
 		return
+	}
+	_, is := data[dataJson.Key]
+	if is {
+		return c.String(http.StatusOK, "already exists")
 	}
 	data[dataJson.Key] = dataJson.Value
 	return c.String(http.StatusOK, "success")
@@ -45,6 +49,6 @@ func setValue(c echo.Context) (err error) {
 
 func deleteValue(c echo.Context) (err error) {
 	key := c.Param("key")
-	delete(data,key)
+	delete(data, key)
 	return c.String(http.StatusOK, "success")
 }
